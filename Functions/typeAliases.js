4b0d38046b60b775e2e01fbf979291657577a109
Abhilash107 <abhilash00003108884@gmail.com>
2024-10-14 21:25:33 +0530

    Type aliases in Ts

---
+++ Functions/typeAliases.js
@@ -0,0 +1,4 @@
+function printCoordinate(pt) {
+    console.log(pt.x);
+    console.log(pt.y);
+}
---
+++ Functions/typeAliases.ts
@@ -0,0 +1,9 @@
+type Point = {
+    x: number;
+    y: number;
+};
+
+function printCoordinate(pt: Point){
+    console.log(pt.x);
+    console.log(pt.y);
+}

3d6dad70ede978395534eabdc74b487cf3acb30a
Abhilash107 <abhilash00003108884@gmail.com>
2024-10-15 18:25:27 +0530

    Type aliases examples

--- Functions/typeAliases.js
+++ Functions/typeAliases.js
@@ -1,4 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function printCoordinate(pt) {
     console.log(pt.x);
     console.log(pt.y);
 }
+var myUser = {
+    _id: "123",
+    name: "hh",
+    email: "a@.dev",
+    isActive: false,
+};
+myUser.name = "asd";
--- Functions/typeAliases.ts
+++ Functions/typeAliases.ts
@@ -7,3 +7,39 @@ function printCoordinate(pt: Point){
     console.log(pt.x);
     console.log(pt.y);
 }
+
+//readonly makes sure that variable can't be over-write.
+type User = {
+    readonly _id: string;
+    name: string;
+    email: string;
+    isActive: boolean;
+    credCardDetails?: number // weird syntax
+}
+
+let myUser: User = {
+    _id: "123",
+    name: "hh",
+    email: "a@.dev",
+    isActive: false,
+}
+
+myUser.name = "asd"
+//myUser._id = "12"//Can't assign to '_id' because it is a read-only property
+
+type cardNumber= {
+    cardnumber : string
+}
+
+type cardDate = {
+    cardDate: string
+}
+
+// combining all together
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+}
+
+
+
+export{}
